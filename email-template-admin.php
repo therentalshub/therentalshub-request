882bd84a46ee6583243173399a5b2d5494aba5cb
Steve Armenakis <support@shopie.gr>
2026-02-16 13:02:42 +0200

    new date format

--- email-template-admin.php
+++ email-template-admin.php
@@ -4,51 +4,51 @@
         <table cellpadding="5" style="text-align:left;border-spacing:10px;border:1px #ccc solid">
             <tr>
                 <th><?=__('Pick-up date/time', 'therentalshub-request');?></th>
-                <td><?=htmlentities($vars->sd, ENT_QUOTES);?> at <?=htmlentities($vars->st, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars(date('d/m/Y', strtotime($vars->sd)), ENT_QUOTES);?> at <?=htmlspecialchars($vars->st, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th><?=__('Drop-off date/time', 'therentalshub-request');?></th>
-                <td><?=htmlentities($vars->ed, ENT_QUOTES);?> at <?=htmlentities($vars->et, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars(date('d/m/Y', strtotime($vars->ed)), ENT_QUOTES);?> at <?=htmlspecialchars($vars->et, ENT_QUOTES);?></td>
             </tr>
             <?php if ($vars->car != 0): ?>
             <tr>
                 <th><?=__('Selected car', 'therentalshub-request');?></th>
-                <td><?=htmlentities($vars->carname, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->carname, ENT_QUOTES);?></td>
             </tr>
             <?php endif; ?>
             <?php if ($vars->pick != 0): ?>
             <tr>
                 <th><?=__('Pick-up location', 'therentalshub-request');?></th>
-                <td><?=htmlentities($vars->plocname, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->plocname, ENT_QUOTES);?></td>
             </tr>
             <?php endif; ?>
             <?php if ($vars->drop != 0): ?>
             <tr>
                 <th><?=__('Drop-off location', 'therentalshub-request');?></th>
-                <td><?=htmlentities($vars->dlocname, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->dlocname, ENT_QUOTES);?></td>
             </tr>
             <?php endif; ?>
             <?php if ($vars->flightname != ''): ?>
             <tr>
                 <th><?=__('Flight nr.', 'therentalshub-request');?></th>
-                <td><?=htmlentities($vars->flightname, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->flightname, ENT_QUOTES);?></td>
             </tr>
             <?php endif; ?>
             <tr>
                 <th><?=__('Customer', 'therentalshub-request');?></th>
-                <td><?=htmlentities($vars->fname, ENT_QUOTES);?> <?=htmlentities($vars->lname, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->fname, ENT_QUOTES);?> <?=htmlspecialchars($vars->lname, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th><?=__('Email', 'therentalshub-request');?></th>
-                <td><?=htmlentities($vars->email, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->email, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th><?=__('Phone', 'therentalshub-request');?></th>
-                <td><?=htmlentities($vars->phone, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->phone, ENT_QUOTES);?></td>
             </tr>
             <tr>
                 <th><?=__('Notes', 'therentalshub-request');?></th>
-                <td><?=htmlentities($vars->notes, ENT_QUOTES);?></td>
+                <td><?=htmlspecialchars($vars->notes, ENT_QUOTES);?></td>
             </tr>
         </table>
     </div>
